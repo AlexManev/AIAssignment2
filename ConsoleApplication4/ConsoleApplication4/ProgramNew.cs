bb053f9f34bfdb2ee6749a888bfc745e4e970f8b
Jakeamorris25 <Jakeamorris25@gmail.com>
2016-05-25 16:01:50 +1000

    Program file to select method

---
+++ ConsoleApplication4/ConsoleApplication4/ProgramNew.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AI_Assignment2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //TruthTable _table = new TruthTable("C:/Users/Jake/Desktop/test1.txt");
+
+            switch (args[0])
+            {
+                case "FC":
+                    //fc
+                    break;
+                case "BC":
+                    //bc
+                    break;
+                case "TT":
+                    TruthTable _table = new TruthTable(args[1]);
+                    break;
+            }
+        }
+    }
+}
