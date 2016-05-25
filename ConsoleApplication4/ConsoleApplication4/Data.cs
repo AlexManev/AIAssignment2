d2c9a1373b616b4ec1e7def85340e0b445da75de
ALEKSANDAR <100586500@student.swin.edu.au>
2016-05-25 12:52:30 +1000

    Solution with BC working

---
+++ ConsoleApplication4/ConsoleApplication4/Data.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace ConsoleApplication4
+{
+    public class Data
+    {
+        private string _implies;
+        private string _id;
+
+        public Data(string id, string implies)
+        {
+            _implies = implies;
+            _id = id.ToLower();
+        }
+
+        public bool AreYou(string id)
+        {
+            id = id.ToLower();
+            return id == _id;
+        }
+
+        public string ID
+        {
+            get { return _id; }
+        }
+        public string Implies
+        {
+            get { return _implies; }
+        }
+    }
+}
